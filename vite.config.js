63a4b98a3f78459a01d3bde3bb8ddf85fcd4b1bb
TanHung189 <hung189204@gmail.com>
2026-03-27 13:26:07 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   // Đặt base là './' hoặc '/Tên-Repo-Của-Bạn/' để sửa lỗi 404 khi deploy lên Github Pages
-  base: './',
-})
+  base: "/Single-Page-CV/",
+});

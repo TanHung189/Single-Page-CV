6bccfd9a6ff401f2256e6535b0941586691ce69a
TanHung189 <hung189204@gmail.com>
2026-03-27 13:18:22 +0700

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Đặt base là './' hoặc '/Tên-Repo-Của-Bạn/' để sửa lỗi 404 khi deploy lên Github Pages
+  base: './',
 })
